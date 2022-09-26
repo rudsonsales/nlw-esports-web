9e28fd843a99fa0f5fd6ec5147f32fab02a7c9ee
rudsonsales <rudsonsales@hotmail.com>
2022-09-26 16:35:23 -0300

    Projeto web

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'Linear-gradient(70.86deg, #9572FC 12.08%, #43E7AD 34.22%, #E1D55D 99.80%)',
+        'game-gradient': 'Linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
+  },
+  plugins: [],
+}
